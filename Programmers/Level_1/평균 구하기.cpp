a2417a54321acf19ba56c430c50ea65b5e0430ba
Gu-nuu <gun0005@naver.com>
2021-08-01 17:38:22 +0900

    Add 평균 구하기 210801

---
+++ Programmers/Level_1/평균 구하기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+double solution(vector<int> arr)
+{
+    double answer = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+        answer += arr[i];
+
+    answer /= arr.size();
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4};
+
+    cout << solution(arr) << endl;
+
+    return 0;
+}
