5860b7ed38019b54d994f62e42402b9451f2d450
Gu-nuu <gun0005@naver.com>
2021-08-02 19:07:01 +0900

    Add 핸드폰 번호 가리기 210802

---
+++ Programmers/Level_1/핸드폰 번호 가리기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string phone_number)
+{
+    string answer = "";
+
+    for (int i = 0; i < phone_number.size() - 4; i++)
+        phone_number[i] = '*';
+
+    answer = phone_number;
+
+    return answer;
+}
+
+int main()
+{
+    string number = "01033334444";
+
+    cout << solution(number) << endl;
+
+    return 0;
+}
