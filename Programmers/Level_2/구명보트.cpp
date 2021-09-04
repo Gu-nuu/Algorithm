b729cd7354d2c8a4e3cb27b480c5efc4598f34c5
Gu-nuu <gun0005@naver.com>
2021-09-04 18:25:58 +0900

    Update 구명보트 210904

--- Programmers/Level_2/구명보트.cpp
+++ Programmers/Level_2/구명보트.cpp
@@ -15,6 +15,7 @@ int solution(vector<int> people, int limit)
 
     // 가장 작은 사람과 가장 무거운 사람과의 몸무게의 합을 계산하여 무게 제한 아래면 2명을 한번에 태울 수 있으므로 left는 증가시키고 right는 감소시켜
     // 두 사람 모두 태운 것으로 간주하며, 무게 제한을 초과하면 right를 감소시켜 무거운 사람 한 명만 태운 것으로 간주한다.
+    // => 가장 가벼운 사람과 같이 보트에 태웠을 때 무게 제한을 초과했다면 그 사람은 무조건 혼자 타야한다!
     while (left <= right)
     {
         if (people[left] + people[right] <= limit)
